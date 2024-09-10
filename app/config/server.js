16a774d80c2633af973c1ca91616fbba0fcb7156
Vincent Sechi <vincentsechi@MacBook-Pro-de-Vincent.local>
2024-09-10 14:56:16 +0200

    push

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8358290fd13e06240d71b74e271c8afc'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6cec0e25-f539-4025-bbe1-17478724d2e5'
+};
